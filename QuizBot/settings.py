5a39453c222f191ba7c2137903fd7a88e3670f5d
fayis <fayispvchelari@gmail.com>
2024-02-11 21:18:09 +0530

    allauth account middleware

--- QuizBot/settings.py
+++ QuizBot/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware', 
 ]
 
 SITE_ID = 1
