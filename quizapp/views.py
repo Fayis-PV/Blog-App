d26985e27df67717c38f9eb821884bb82487853e
fayis <fayispvchelari@gmail.com>
2024-02-12 12:38:27 +0530

    views changed for models and forms

--- quizapp/views.py
+++ quizapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Blog, Comment, UserProfile, Category, Tag
-from .forms import BlogForm, CommentForm, UserProfileForm, CategoryForm, TagForm, UserForm
+from .models import Blog, Comment, UserProfile, Category, Tag, Contact, Reply
+from .forms import BlogForm, CommentForm, UserProfileForm, CategoryForm, TagForm, UserForm, ContactForm, ReplyForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
@@ -11,6 +11,8 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 User = get_user_model()
